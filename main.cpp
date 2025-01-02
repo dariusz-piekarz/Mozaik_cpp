a9a6e9e189733c2682c60e5413ad1f0ce66684db
Dariusz <162720843+dariusz-piekarz@users.noreply.github.com>
2025-01-02 20:06:21 +0100

    Update main.cpp
    Switched to a function where config.json path is manually provided by the user.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     clock_t start = std::clock();
-    mozaik();
+    mozaik2();
     clock_t end = std::clock();
     spdlog::info("Mozaik total time execution: " + elapse_time(start, end));
     return 0;
