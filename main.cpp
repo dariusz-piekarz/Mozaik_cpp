fedad9a82b4b6bbf73895ba801a6a6ef155049c2
Dariusz <162720843+dariusz-piekarz@users.noreply.github.com>
2025-01-08 22:31:35 +0100

    Applied Cpp coding standards

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-#include "core.hpp"
+#define _CRT_SECURE_NO_WARNINGS // switches off warnings in glob.hpp
+
+
 #include <ctime>
+
+
 #include <spdlog/spdlog.h>
+
+
+#include "core.hpp"
+
 
 
 int main()
@@ -8,6 +16,6 @@ int main()
     clock_t start = std::clock();
     mozaik2();
     clock_t end = std::clock();
-    spdlog::info("Mozaik total time execution: " + elapse_time(start, end));
+    spdlog::info("Mozaik total time execution: " + elapseTime(start, end));
     return 0;
 }
